125c403896999103286062db9c989cbb35bc389f
muhammad fajar <invasionfajar@gmail.com>
2021-08-31 22:01:57 +0700

    Setting Permission Done

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','id_pasien'
+        'name', 'email', 'password','id_pasien','role_id'
     ];
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
         <x-jet-validation-errors class="mb-3" />
 
         <div class="card-body">
-            <form method="POST" action="{{ route('register') }}">
+            <form method="POST" action="{{ route('register') }}" x-data="{role_id: 2}">
                 @csrf
 
                 <div class="form-group">
@@ -40,6 +40,29 @@
                     <x-jet-input class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
                 </div>
 
+                <div class="mt-4">
+                    <x-jet-label for="role_id" value="{{ __('Register as:') }}" />
+                    <select name="role_id" x-model="role_id" class="form-control">
+                        <option value="2">Bidan</option>
+                        <option value="3">Administrasi</option>
+                    </select>
+                </div>
+{{--
+                <div class="mt-4" x-show="role_id == 2">
+                    <x-jet-label for="student_address" value="{{ __('Address') }}" />
+                    <x-jet-input id="student_address" class="block mt-1 w-full" type="text" :value="old('student_address')" name="student_address" />
+                </div>
+
+                <div class="mt-4" x-show="role_id == 2">
+                    <x-jet-label for="student_licence_number" value="{{ __('Licence Number') }}" />
+                    <x-jet-input id="student_licence_number" class="block mt-1 w-full" type="text" :value="old('student_licence_number')" name="student_licence_number" />
+                </div>
+
+                <div class="mt-4" x-show="role_id == 3">
+                    <x-jet-label for="teacher_qualifications" value="{{ __('Qualifications') }}" />
+                    <x-jet-input id="teacher_qualifications" class="block mt-1 w-full" type="text" :value="old('teacher_qualifications')" name="teacher_qualifications" />
+                </div> --}}
+
                 @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                     <div class="form-group">
                         <div class="custom-control custom-checkbox">
@@ -54,7 +77,7 @@
                     </div>
                 @endif
 
-                <div class="mb-0">
+                <div class="mb-0 mt-3">
                     <div class="d-flex justify-content-end align-items-baseline">
                         <a class="text-muted mr-3 text-decoration-none" href="{{ route('login') }}">
                             {{ __('Already registered?') }}
