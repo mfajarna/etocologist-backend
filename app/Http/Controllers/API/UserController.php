8b35730b31dec7fe827d7d73c632cbfecad0296c
muhammad fajar <invasionfajar@gmail.com>
2021-09-02 05:26:19 +0700

    a

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -51,6 +51,6 @@ class UserController extends Controller
 
     public function fetch(Request $request)
     {
-        return ResponseFormatter::success(Auth::user(), 'Data profile user berhasil diambil!');
+        return ResponseFormatter::success(Auth::user()->dataUser, 'Data profile user berhasil diambil!');
     }
 }
