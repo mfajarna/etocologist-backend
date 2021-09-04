0bfcfb67813256240a668640ceb362af1965bbdd
muhammad fajar <invasionfajar@gmail.com>
2021-09-04 22:00:34 +0700

    API get ibu

--- app/Http/Controllers/API/DataibuController.php
+++ app/Http/Controllers/API/DataibuController.php
@@ -13,10 +13,29 @@ class DataibuController extends Controller
     public function all(Request $request)
     {
         $limit = $request->input('limit', 100);
+        $id = $request->input('id');
 
-        $model = Riwayatkehamilan::with('ibu')->all();
+        $model = Riwayatkehamilan::with('ibu')
+                            ->where('id_ibu', Auth::user()->id);
 
+        if($id)
+        {
+            $model = Riwayatkehamilan::with('ibu')->find($id);
 
+            if($model)
+            {
+                return ResponseFormatter::success(
+                    $model,
+                    'Data Transaksi Berhasil Di Ambil'
+                );
+            }else{
+                return ResponseFormatter::error([
+                    null,
+                    'Data Transaksi Tidak Ada',
+                    404
+                ]);
+            }
+        }
 
         return ResponseFormatter::success(
             $model->paginate($limit),
